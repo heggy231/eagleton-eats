ab8bc5018c55ca07de10320c9f096def88bbab83
Jared Davie <Jared.Davie@alumni.ou.edu>
2021-08-02 15:54:19 -0500

    mapping is working need to add images

--- react-ui/src/routes/menu/menu.jsx
+++ react-ui/src/routes/menu/menu.jsx
@@ -3,19 +3,16 @@ import foods from "../../data/food";
 const Menu = () => {
   return (
     <div>
-      {foods.map((food) => {
-        //   return (
-        //     <div>
-        //       {/* <img src={food.image} /> */}
-        //       <h2>{food.item}</h2>
-        //       <p>{food.price}</p>
-        //     </div>
-        //   );
-        // })}
-        console.log(food);
-        return <p>Food</p>;
+      {foods.foods.map((food) => {
+        return (
+          <div>
+            {/* <img src={food.image} /> */}
+            <h2>{food.item}</h2>
+            <p>{food.price}</p>
+          </div>
+        );
       })}
-      <h1>Food Pics</h1>
+      )<h1>Food Pics</h1>
     </div>
   );
 };
