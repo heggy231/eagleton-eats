fa525217e2a25a2ef6ecd4895628172bff4ca1bd
Heggy Castaneda <heggyy@gmail.com>
2021-08-19 20:31:56 -0400

    add the gitignore

--- react-ui/src/routes/about/about.jsx
+++ react-ui/src/routes/about/about.jsx
@@ -1,16 +1,30 @@
 import Container from "react-bootstrap/Container";
-import { Col, Image } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
+import "./style.css"
+import Heggy from "./heggy-github.png";
 
 const About = () => {
   return (
     <div id="portrait">
     <Container>
-      <Col xs={6} md={4}>
-        <Image
-          src="https://media-exp1.licdn.com/dms/image/C4E03AQFaLCpu0JWG6A/profile-displayphoto-shrink_800_800/0/1624842284126?e=1633564800&v=beta&t=seaxieLlHFi2qEo6CEoDHxXzAJR0aKKy6cHjW41usMU"
-          roundedCircle
-        />
-      </Col>
+      <Row>
+        <Col xs={7} md={6} lg={3}>
+          <div className="image-container">
+            <Image
+              src="https://media-exp1.licdn.com/dms/image/C4E03AQFaLCpu0JWG6A/profile-displayphoto-shrink_800_800/0/1624842284126?e=1633564800&v=beta&t=seaxieLlHFi2qEo6CEoDHxXzAJR0aKKy6cHjW41usMU"
+              roundedCircle
+            />
+          </div>
+        </Col>
+        <Col xs={7} md={6} lg={3}>
+          <div className="image-container">
+            <Image
+              src={Heggy}
+              roundedCircle
+            />
+          </div>
+        </Col>
+      </Row>
     </Container>
     <div id="about">
         Junior Web-Developer, with experience with Java-script and React-Redux.
