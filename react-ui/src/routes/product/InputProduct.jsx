5c7d34c55447a3e6b95712b6fd24d0042f17faa4
Heggy Castaneda <heggyy@gmail.com>
2021-08-21 12:52:49 -0400

    response to backend 200

--- react-ui/src/routes/product/InputProduct.jsx
+++ react-ui/src/routes/product/InputProduct.jsx
@@ -6,11 +6,26 @@ const InputProduct = () => {
 
   const handleInputChange = (e) => setName(e.target.value);
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = {name};
+      const response = await fetch('http://localhost:8080/product', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+      console.log('my input response', response);
+    } catch (error){
+      console.error(error.message);
+    }
+  }
+
   return (
     <>
       <div className="container">
         <h1 className="text-center">input product</h1>
-        <form className="d-flex mt-3">
+        <form className="d-flex mt-3" onSubmit={onSubmitForm}>
           <input type="text" className="form-control" placeholder='Enter Product Name' onChange={handleInputChange}/>
           <button className="btn btn-warning">Add Product</button>
         </form>
