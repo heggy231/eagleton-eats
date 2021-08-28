be4d66bf68a483e580b651413fdcef81106c4e4c
Heggy Castaneda <heggyy@gmail.com>
2021-08-28 13:59:23 -0400

    404

--- react-ui/src/components/NotFoundPage.jsx
+++ react-ui/src/components/NotFoundPage.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const jumboStyle = {
+    backgroundColor: "rgb(229 75 182 / 38%)",
+    marginTop: "-1rem",
+    color: "#00ffc6",
+  };
   return (
-    <div>
+    <div className="jumbotron" style={jumboStyle}>
       <h2>404 Page Not Found</h2>
       <p>
-        <Link to="/">Go to Home Page</Link>
+        <Link className="btn btn-primary" to="/">
+          Take me back to Home Page
+        </Link>
       </p>
     </div>
   );
